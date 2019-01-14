457fa2b0c7ef9a207e58e8bd1deeb6be4f377c3a
Imad <ibelkebir@stuy.edu>
2019-01-14 13:29:28 -0500

    testing networking

--- Tetris.java
+++ Tetris.java
@@ -9,13 +9,9 @@ public class Tetris extends JFrame implements ActionListener{
   private JLabel level;
   private JLabel heldPiece;
   private JLabel nextPiece;
-  private startScreen screen;
-  private JButton inst;
-  private instructions instructScreen;
   private Timer timer;
   private JLabel  gameover;
   private gameOverScreen over;
-  private JButton start;
   private Board  matrix;
   private JPanel sidebar;
   private predict predictor;
@@ -25,28 +21,15 @@ public class Tetris extends JFrame implements ActionListener{
     this.setSize(800,1000);
     this.setLocation(100,0);
     this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-    start=new JButton("START");
     predictor=new predict();
     held=new hold();
-    screen=new startScreen("image.png");
     over=new gameOverScreen();
-    instructScreen=new instructions("image.png");
-    inst= new JButton("Instructions");
     // The timer is used to check if the game is over indicating whether the gameover method should be invoked
     timer=new Timer(10,this);
     timer.setActionCommand("gameover?");
     timer.start();
 
-    start.setFocusable(false);
-    inst.setFocusable(false);
-
-    inst.addActionListener(this);
-    start.addActionListener(this);
-
     pane = this.getContentPane();
-    start.setPreferredSize(new Dimension(200, 100));
-    inst.setPreferredSize(new Dimension(200, 50));
-    pane.setLayout(new BorderLayout());
 
     score=new JLabel("Score:0 ");
     gameover=new JLabel("GAMEOVER");
@@ -62,21 +45,11 @@ public class Tetris extends JFrame implements ActionListener{
     heldPiece.setFont(new Font("Serif",Font.PLAIN,30));
     nextPiece.setFont(new Font("Serif",Font.PLAIN,30));
     matrix=new Board(this, pid);
-    pane.add(screen, BorderLayout.PAGE_START);
-    GridBagConstraints gbc=new GridBagConstraints();
-    gbc.gridy=1;
-    gbc.weighty=1;
-
-    gbc.anchor=GridBagConstraints.CENTER;
-    screen.add(start, gbc);
-    gbc.gridy=2;
-    screen.add(inst, gbc);
-
 
     pane.add(matrix);
     pane.add(sidebar);
-    matrix.setVisible(false);
-    sidebar.setVisible(false);
+    matrix.setVisible(true);
+    sidebar.setVisible(true);
     sidebar.add(nextPiece);
     sidebar.add(predictor);
     sidebar.add(heldPiece);
@@ -97,7 +70,6 @@ public class Tetris extends JFrame implements ActionListener{
     over.add(gameover);
     over.add(score);
     over.add(level);
-    over.add(start);
   }
   public void actionPerformed(ActionEvent e){
     String s=e.getActionCommand();
@@ -108,36 +80,6 @@ public class Tetris extends JFrame implements ActionListener{
 
         }
       }catch(NullPointerException E){}
-    }
-    //The instructions button shows the instructions for new players unfamiliar with the game
-    if(s.equals("Instructions")){
-      screen.setVisible(false);
-      GridBagConstraints gbc=new GridBagConstraints();
-      gbc.gridy=1;
-      start.setMinimumSize(new Dimension(200, 100));
-      instructScreen.add(start,gbc);
-      pane.add(instructScreen,BorderLayout.PAGE_START);
-    }
-    // The start button once pressed removes the start button and if the player is starting again the gameover label, and makes the matrix and sidebar visible, while also changing the panes layout.
-    if(s.equals("START")){
-	    pane.remove(over);
-      pane.remove(instructScreen);
-	    pane.setLayout(new GridLayout());
-      pane.remove(start);
-	    pane.remove(screen);
-	    pane.remove(score);
-      pane.remove(level);
-      matrix.setVisible(true);
-	    matrix.requestFocus();
-      sidebar.add(nextPiece);
-      sidebar.add(predictor);
-      sidebar.add(heldPiece);
-	    sidebar.add(held);
-	    sidebar.add(score);
-	    sidebar.add(level);
-      predictor.setVisible(true);
-      held.setVisible(true);
-	    sidebar.setVisible(true);
     }
 
   }

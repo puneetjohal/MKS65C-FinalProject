c639af295f8831c6067149266e540a6f6f34f8e8
ibelkebeer <ibelkebir@stuy.edu>
2019-01-15 03:29:25 -0500

    Still trying to fix pipes

--- pipe.java
+++ pipe.java
@@ -2,17 +2,17 @@ import java.util.*;
 import java.nio.*;
 
 public class pipe{
-  private Path path1, path2;
+  private nio.Path path1, path2;
   public pipe(int pid_in, int pid_out){
-    path1 = Paths.get("" + pid_in);
-    path2 = Paths.get("" + pid_out);
+    path1 = nio.Paths.get("" + pid_in);
+    path2 = nio.Paths.get("" + pid_out);
   }
   public boolean hasNext(){
     boolean next = false;
     return next;
   }
   public String nextLine(){
-    String input = new String(Files.readAllBytes(path1));
+    String input = new String(nio.Files.readAllBytes(path1));
     return input;
   }
   public void writeOut(String output){

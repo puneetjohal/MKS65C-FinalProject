4f2c1ab6ee678b107d74680a07088d2ad034a156
ibelkebeer <ibelkebir@stuy.edu>
2019-01-15 02:45:20 -0500

    Still trying to fix pipes

--- pipe.java
+++ pipe.java
@@ -2,19 +2,19 @@ import java.util.*;
 import java.io.*;
 
 public class pipe{
-  private FileWriter out;
-  private Scanner in;
+  private OutputStreamWriter out;
+  private BufferedReader in;
   private File f1, f2;
   public pipe(int pid_in, int pid_out){
     f1 = new File("" + pid_in);
     f2 = new File("" + pid_out);
+    in = new BufferedReader(new FileReader(f1));
+    out = new OutputStreamWriter(new FileOutputStream(f2));
   }
   public boolean hasNext(){
     boolean next = false;
     try{
-      in = new Scanner(f1);
-      next = in.hasNext();
-      in.close();
+      next = in.ready();
     }catch(Exception e){
     }
     return next;
@@ -22,19 +22,15 @@ public class pipe{
   public String nextLine(){
     String input = null;
     try{
-      in = new Scanner(f1);
-      input = in.nextLine();
-      in.close();
+      next = in.readLine();
     }catch(Exception e){
     }
     return input;
   }
   public void writeOut(String output){
     try{
-      out = new FileWriter(f2);
       out.write(output);
       out.flush();
-      out.close();
     }catch(Exception e){
     }
   }

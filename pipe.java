54051c288276343d85a2659b42f2301e9b155168
Imad <ibelkebir@stuy.edu>
2019-01-15 13:54:25 -0500

    pipes work?

--- pipe.java
+++ pipe.java
@@ -1,15 +1,18 @@
 import java.util.*;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.Path;
 
 public class pipe{
   private Path path1, path2;
+  private File f;
   public pipe(int pid_in, int pid_out){
     path1 = Paths.get("" + pid_in);
     path2 = Paths.get("" + pid_out);
     System.out.println(path1);
     System.out.println(path2);
+    f = new File(pid_out);
   }
   public boolean hasNext(){
     boolean next = false;
@@ -25,7 +28,12 @@ public class pipe{
   }
   public void writeOut(String output){
     try{
-      Files.write(path2, output.getBytes("UTF-8"));
+      //Files.write(path2, output.getBytes("UTF-8"));
+      FileWriter out = new FileWriter(f);
+      out.write(output);
+      out.flush();
+      out.close();
+      System.out.println("Sent message: " + output);
     }catch(Exception e){
     }
   }

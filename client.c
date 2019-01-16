752ff9a9ca424a2b0211aba1487b4544f3badadf
Imad <ibelkebir@stuy.edu>
2019-01-16 12:54:23 -0500

    tournament

--- client.c
+++ client.c
@@ -105,7 +105,7 @@ int main(int argc, char **argv) {
       }
     }
 
-    if (strcmp(gameMode,"2")==0) {
+    if (strcmp(gameMode,"3")==0) {
       char javapipeIN[10];
       char javapipeOUT[10];
       sprintf(javapipeIN, "%d", getpid() + 10);
@@ -150,9 +150,10 @@ int main(int argc, char **argv) {
           read(server_socket, sig, 10);
           fifo = open(javapipeOUT, O_WRONLY);
           if(strcmp(sig, "0") == 0){
-            printf("You Won!\n");
+            printf("You Won!\nYou made it to round 2!\n");
             write(fifo, sig, strlen(sig));
             close(fifo);
+            break;
           }
           if(strcmp(sig, "1") == 0 || strcmp(sig, "2") == 0 || strcmp(sig, "3") == 0 || strcmp(sig, "4") == 0){
             printf("Your opponent attacked you!\n");
@@ -160,6 +161,57 @@ int main(int argc, char **argv) {
             strcpy(sig, "");
           }
           close(fifo);
+        }
+        if (argc == 2)
+          server_socket = client_setup( argv[1] );
+        else
+          server_socket = client_setup( TEST_IP );
+        int fifo;
+        int f = fork();
+        if(!f){
+          char* command[4];
+          command[0] = "java";
+          command[1] = "Tetris";
+          command[2] = javapipeIN;
+          command[3] = NULL;
+          printf("\nwaiting for other player to join\n");
+          while(read(server_socket, buffer, sizeof(buffer)) == -1);
+          printf("starting game...\n");
+          execvp(command[0], command);
+        }else{
+        }
+        f = fork();
+        if(f){
+          while(1){
+            fifo = open(javapipeIN, O_RDONLY);
+            char sig[10];
+            read(fifo, sig, 10);
+            close(fifo);
+            write(server_socket, sig, sizeof(sig));
+            if(strcmp(sig, "0") == 0){
+              printf("You Lost!\n");
+              exit(0);
+            }
+          }
+        }else{
+          fcntl(server_socket, F_SETFL, O_NONBLOCK);
+          while(1){
+            char sig[10];
+            read(server_socket, sig, 10);
+            fifo = open(javapipeOUT, O_WRONLY);
+            if(strcmp(sig, "0") == 0){
+              printf("You Won The Tournament!\n");
+              write(fifo, sig, strlen(sig));
+              close(fifo);
+              exit(0);
+            }
+            if(strcmp(sig, "1") == 0 || strcmp(sig, "2") == 0 || strcmp(sig, "3") == 0 || strcmp(sig, "4") == 0){
+              printf("Your opponent attacked you!\n");
+              write(fifo, sig, strlen(sig));
+              strcpy(sig, "");
+            }
+            close(fifo);
+          }
         }
       }
     }
